2ca30e624f3804689e2146475ea120909f181320
Mawnzie <majawihe@hotmail.com>
2025-06-03 15:02:28 +0200

    Add project files.

---
+++ AssetTracking/Asset.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyMiniProject
+{
+
+
+    public class Asset : IComparable
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public float Price { get; set; }
+        public float LocalPrice { get; set; }
+        public Location Office { get; set; }
+        public Asset(Location office, string brand, string model, DateTime purchaseDate, float price)
+        {
+            if (price < 0)
+            {
+                throw new ArgumentOutOfRangeException("Price has to be greater than 0.");
+            }
+            Office = office;
+            Price = price;
+            switch (Office)
+            {
+                case Location.Sweden:
+                    LocalPrice = Price * 9.5f;
+                    break;
+
+                case Location.Germany:
+                    LocalPrice = Price * 0.85f;
+                    break;
+
+                default:
+                    LocalPrice = price;
+                    break;
+            }
+            Brand = brand;
+            Model = model;
+            PurchaseDate = purchaseDate;
+
+
+
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Office}".PadRight(15) + $"{this.GetType().Name}".PadRight(15) + $"{Brand}".PadRight(15) + $"{Model}".PadRight(15) + $"{Price}".PadRight(15) + $"{LocalPrice}".PadRight(15) + $"{PurchaseDate}".PadRight(15);
+        }
+
+        public int CompareTo(object obj)
+        {
+
+            Asset otherAsset = (Asset)obj; //Try casting obj to Asset.
+
+            if (this.GetType().Name[0] > otherAsset.GetType().Name[0])
+            {
+                return 1;
+                //If the first name of the class
+            }
+            else if (this.GetType().Name[0] < otherAsset.GetType().Name[0])
+            {
+                return -1;
+            }
+            else
+            {
+                if (this.PurchaseDate > otherAsset.PurchaseDate)
+                {
+                    return 1;
+                }
+                else if (this.PurchaseDate < otherAsset.PurchaseDate)
+                {
+                    return -1;
+                }
+                else { return 0; }
+            }
+
+        }
+    }
+        public class Computer : Asset
+    {
+        public AssetType AssetType { get; set; }
+
+        public Computer(Location office, string brand, string model, DateTime purchaseDate, float price) : base(office, brand, model, purchaseDate, price)
+        { }
+
+    }
+
+    public class MobilePhone : Asset
+    {
+
+        public MobilePhone(Location office, string brand, string model, DateTime purchaseDate, float price) : base(office,brand, model, purchaseDate, price)
+        { }
+
+    }
+
+
+    public enum AssetType
+    {
+        Computer,
+        MobilePhone,
+    }
+
+    public enum Location
+    {
+        Germany,
+        Sweden,
+        UnitedStates,
+    }
+}
---
+++ AssetTracking/Program.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using WeeklyMiniProject;
+
+List<Asset> assets = new List<Asset>();
+
+
+while (true){
+
+
+    Console.WriteLine("Enter office location:");
+    string officeinput = Console.ReadLine();
+
+    if (Enum.TryParse<Location>(officeinput, out Location office))
+    {
+        Console.WriteLine($"Parsed successfully: {office}");
+    }
+    else
+    {
+        Console.WriteLine("Invalid input.");
+    }
+
+    Console.WriteLine("Enter asset type:");
+
+    string input = Console.ReadLine();
+    if (Enum.TryParse<AssetType>(input, out AssetType asset))
+    {
+        Console.WriteLine($"Parsed successfully: {asset}");
+    }
+    else
+    {
+        Console.WriteLine("Invalid input.");
+    }
+
+
+    Console.WriteLine("Enter brand:");
+    string? brand = Console.ReadLine();
+
+    Console.WriteLine("Enter Model:");
+    string? model = Console.ReadLine();
+
+
+    Console.WriteLine("Enter Purchase date (MM/dd/yyyy):");
+    string enteredString = Console.ReadLine();
+    DateTime purchaseDate = DateTime.Parse(enteredString);
+
+    Console.WriteLine("Enter Purchase price:");
+    float price = float.Parse(Console.ReadLine());
+
+
+    
+    switch (asset)
+    {
+        case AssetType.Computer:
+            var computer = new Computer(office, brand, model, purchaseDate, price);
+            assets.Add(computer);
+            break;
+
+
+        case AssetType.MobilePhone:
+            var phone = new MobilePhone(office, brand, model, purchaseDate, price);
+            assets.Add(phone);
+            break;
+
+        default:
+            throw new ArgumentOutOfRangeException();
+
+    }
+
+    Console.WriteLine("Enter another product? y/n");
+
+    if (Console.ReadLine() != "y")
+    {
+        break;
+    }
+}
+
+
+Console.WriteLine("Office".PadRight(15) + "Asset".PadRight(15) + "Brand".PadRight(15) + "Model".PadRight(15) + "Price (USD)".PadRight(15) + "Local price".PadRight(15) + "PurchaseDate".PadRight(15));
+
+assets.Sort(); //Since we have implemented the compareTo-method of the IComparable interface in the Asset-class this works.
+
+
+foreach (var asset in assets)
+{
+    DateTime threshold = asset.PurchaseDate.AddYears(3);
+    TimeSpan timeUntilThreeYears = threshold - DateTime.Now;
+
+    if (timeUntilThreeYears.TotalDays <= 90 && timeUntilThreeYears.TotalDays >= 0)
+    {
+        // Less than 3 months to 3-year mark
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(asset);
+        Console.ResetColor();
+    }
+    else if (timeUntilThreeYears.TotalDays <= 180 && timeUntilThreeYears.TotalDays > 90)
+    {
+        // Between 3–6 months to 3-year mark
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(asset);
+        Console.ResetColor();
+    }
+    else
+    {
+        // More than 6 months away or already passed 3 years
+        Console.WriteLine(asset);
+    }
+}
+
+
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+
+
